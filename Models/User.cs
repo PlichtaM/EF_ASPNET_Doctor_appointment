4a929354d5aa372eaede598a14d8b167c181b61d
PlichtaM <mapl.plichta@gmail.com>
2023-06-03 21:37:54 +0200

    innit

---
+++ Models/User.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication2.Models
+{
+    public class User : IdentityUser
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name is required.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Username is required.")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public List<Appointment> Appointments { get; set; } = new List<Appointment>();
+    }
+}
